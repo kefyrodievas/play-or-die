84f848e84c1df680f956d047e815868cfec40846
Deima <deimantevitk@gmail.com>
2026-02-23 23:02:46 +0200

    judėjimas
    pasisuka į tą pusę į kurią spaudžiama, nieko nespaudžiant idle animation

--- scenes/samurai.gd
+++ scenes/samurai.gd
@@ -6,11 +6,18 @@ const JUMP_VELOCITY = -600.0
 
 
 func _physics_process(delta: float) -> void:
+	
+	# Add animation
+	if velocity.x > 1 or velocity.x < -1:
+		$AnimatedSprite2D.play("run")
+	else:
+		$AnimatedSprite2D.play("idle")
+		
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	else:
-		$AnimatedSprite2D.play("run")
+	#else:
+		#$AnimatedSprite2D.play("run")
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -25,3 +32,8 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+	if direction == 1.0:
+		$AnimatedSprite2D.flip_h = false
+	elif direction == -1.0:
+		$AnimatedSprite2D.flip_h = true
