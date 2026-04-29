ecdfe42927e7c7b9e1a20062e56cf056deb5e2cd
Ignas Kižys <ignas.kizys@tutanota.com>
2026-04-29 20:37:13 +0300

    added basic powerup timers

--- scenes/cd_timer.gd
+++ scenes/cd_timer.gd
@@ -12,12 +12,12 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	$p_bar.value = timer.time_left
-	print(timer.time_left)
+	#print(timer.time_left)
 	if timer.time_left == 0:
 		self.queue_free()
 	
 func set_timer(timer_ : Timer):
-	print("AAAAAAAAAAAAAAAAAAAAa\n")
+	#print("AAAAAAAAAAAAAAAAAAAAa\n")
 	timer = timer_
 	$p_bar.max_value = timer.wait_time 
 	
